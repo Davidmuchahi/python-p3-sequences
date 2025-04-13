662e5f5812635dea8b6659cf27b193712e42ce56
David Muchahi <muchahi853@gmail.com>
2025-04-14 00:09:22 +0300

    Finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length==0:
+        print([])
+        return
+    
+    sequence=[]
+    for i in range(length):
+        if i==0:
+            sequence.append(0)
+
+        elif i==1:
+            sequence.append(1)
+        else:
+            next_number=sequence[i-1]+sequence[i-2]
+            sequence.append(next_number)
+    
+    print(sequence)
+
+              
+print_fibonacci(0)
+print_fibonacci(1)  
+print_fibonacci(2)  
+print_fibonacci(5)  
+print_fibonacci(10) 
+
